011adf3d2eb37decf6074c7d4c1087f872c75e82
Felipe F. Rocha <felipefonsecarocha@gmail.com>
2020-12-30 21:29:19 -0300

    DATASUS - tratamente de dados de municipio e ufs e data

--- treat_datasus.py
+++ treat_datasus.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import csv
+from unidecode import unidecode
 
 datasus_path = os.getcwd()
 
@@ -11,6 +12,29 @@ def read_db_file_csv(path):
     with open(file=path, encoding='ISO-8859-1') as datasus_csv:
         dataframe = pd.read_csv(datasus_csv, delimiter=';', low_memory=False)
     return dataframe.copy()
+
+
+def read_csv_municipios():
+    cod_municipio = None
+    with open(file=f'{datasus_path}/data/cod_municipio.csv', mode='r', newline='') as file_cod_municipio:
+        cod_municipio = pd.read_csv(file_cod_municipio, delimiter=',', low_memory=False)
+        cod_municipio['Código Município Completo'] = cod_municipio['Código Município Completo'].apply(
+            lambda x: int(str(x)[0:6]))
+        cod_municipio['Nome_Município'] = cod_municipio['Nome_Município'].apply(lambda x: unidecode(x).upper())
+        cod_municipio['Nome_UF'] = cod_municipio['Nome_UF'].apply(lambda x: unidecode(x).upper())
+        cod_municipio.rename(
+            columns={'Nome_Município': 'nome_municipio', 'Código Município Completo': 'cod_municipio', 'Nome_UF': 'uf',
+                     'UF': 'cod_uf'}, inplace=True)
+        cod_municipio = cod_municipio[['cod_municipio', 'nome_municipio']]
+    return cod_municipio.copy()
+
+
+def read_csv_uf():
+    cod_uf = None
+    with open(file=f'{datasus_path}/data/cod_uf.csv', mode='r', newline='') as file_cod_uf:
+        cod_uf = pd.read_csv(file_cod_uf, delimiter=',', low_memory=False)
+        cod_uf = cod_uf[['cod_uf', 'sg_uf']]
+    return cod_uf.copy()
 
 
 # with open(file=f'{datasus_path}/new_consolided_file.csv', mode='w', newline='') as csvfile:
@@ -40,27 +64,26 @@ if __name__ == "__main__":
         datasus_length = len(new_datasus) - 1
         datasus_columns = new_datasus[datasus_length].columns
 
-        [print(f'Tamanho do arquivo {files[i]}: {len(new_datasus[i])}') for i in range(datasus_length+1)]
+        [print(f'Tamanho do arquivo {files[i]}: {len(new_datasus[i])}') for i in range(datasus_length + 1)]
 
-        merged_datasus= None
-        for i in range(datasus_length+1):
+        merged_datasus = None
+        for i in range(datasus_length + 1):
             if i >= datasus_length:
                 merged_datasus = merged_datasus.append(new_datasus[i])
                 print(f'Fim dos arquivos {i}')
                 break
 
-            difference = set(new_datasus[i].columns) ^ (set(new_datasus[i+1].columns))
+            difference = set(new_datasus[i].columns) ^ (set(new_datasus[i + 1].columns))
 
             new_datasus[i] = new_datasus[i].drop(columns=difference, errors='ignore')
-            new_datasus[i+1] = new_datasus[i+1].drop(columns=difference, errors='ignore')
+            new_datasus[i + 1] = new_datasus[i + 1].drop(columns=difference, errors='ignore')
 
             if i == 0:
                 merged_datasus = new_datasus[i].copy()
             else:
-                merged_datasus = merged_datasus.append(new_datasus[i],ignore_index=True)
+                merged_datasus = merged_datasus.append(new_datasus[i], ignore_index=True)
 
-
-            print(f'{i}, {i+1}: length {len(new_datasus[i])}, {len(new_datasus[i+1])}')
+            print(f'{i}, {i + 1}: length {len(new_datasus[i])}, {len(new_datasus[i + 1])}')
             # print(difference)
             # print(set(new_datasus[i].columns) ^ (set(new_datasus[i+1].columns)))
             print(f'Size merged {len(merged_datasus)} Index file {i}')
@@ -71,10 +94,24 @@ if __name__ == "__main__":
         print(f'10 Primeiras linhas:\n {merged_datasus.head(10)}')
         print(f'10 Ultimas linhas:\n {merged_datasus.tail(10)}')
 
-        merged_datasus.to_csv(path_or_buf=csvfile,index=False)
+        cod_municipio = read_csv_municipios()
+        cod_uf = read_csv_uf()
 
+        print(cod_municipio.head(10))
+        print(cod_uf.head(10))
+
+        merged_datasus['SG_UF'].replace(dict(zip(cod_uf.cod_uf, cod_uf.sg_uf)), inplace=True)
+        merged_datasus['ID_MUNICIP'].replace(dict(zip(cod_municipio.cod_municipio, cod_municipio.nome_municipio)),
+                                             inplace=True)
+
+        merged_datasus['DT_NOTIFIC'] = merged_datasus['DT_NOTIFIC'].apply(
+            lambda x: str(x)[3:])
+
+        print(merged_datasus[['SG_UF', 'ID_MUNICIP']].head(10))
+
+        merg
+
+        merged_datasus.to_csv(path_or_buf=csvfile, index=False)
 
     # with open(file=f'{os.getcwd()}/headers19') as headers:
     #     read = (headers)
-
-
