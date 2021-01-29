7b4faa7e5593f4479aafb461c2c7c6b6de112a45
Felipe F. Rocha <felipefonsecarocha@gmail.com>
2021-01-29 02:12:19 -0300

    Multprocesso em todas as rotinas

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 from scriptAgrupamento import *
 from treat_datasus import *
-
+from orcamento_mg import *
 ###
 # Configure logs
 ###
@@ -8,15 +8,25 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
+
+
 ###
 # END - Configure logs
 ###
 
+def run_routine(data: str, routine):
+    log.info(f'Consolidating {data} Files')
+    routine()
+    log.info(f'{data} Files Consolidated')
+
+
 if __name__ == "__main__":
     log.info('Initializing routines')
-    log.info('Consolidating Data Sus Files')
-    consolidate_datasus()
-    log.info('Data Sus Files Consolidated')
+
+    processes = [multiprocessing.Process(target=run_routine, args=('Data Sus', consolidate_datasus,),daemon=False)
+                ,multiprocessing.Process(target=run_routine, args=("Data Budget", consolidate_budget,),daemon=False)]
+    [res.start() for res in processes]
+    [res.join() for res in processes]
     log.info('Consolidating Budgeting')
     create_splited_columns()
     log.info('Budget Consolidated')
--- orcamento_mg.py
+++ orcamento_mg.py
@@ -4,8 +4,7 @@ Created on Tue Dec 22 20:45:01 2020
 
 @author: tiago
 """
-import numpy as np
-import csv
+import logging
 
 import pandas as pd
 
@@ -14,35 +13,35 @@ import pandas as pd
 ###
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 logging.basicConfig(format=FORMAT)
 ###
 # END - Configure logs
 ###
 
+def consolidate_budget():
 
-pd.options.display.float_format = "{:.2f}".format
-size = 10;
-ano_inicio = 2009
-teste2 = [0] * size
-for x in range(size):
-    
-    orcamento_mg = pd.read_csv("data/orcamento/orcamento_mg_{}.csv".format(ano_inicio),sep=';')
-    
-    
-    orcamento_mg["Valor Total"] = orcamento_mg["Valor Total"].replace('[.]', '', regex=True).replace('[,]', '.', regex=True).replace('[R$]', '', regex=True).astype(float)
-    orcamento_mg["DATA OB"] = orcamento_mg["DATA OB"].apply(lambda x: x[3:10])
-    group = orcamento_mg['Valor Total'].groupby(orcamento_mg['MUNICIPIO']) 
-    
-    grouped_single = orcamento_mg.groupby(['MUNICIPIO','DATA OB']).agg({'Valor Total': ['sum', 'min', 'max']})
-    grouped_single = grouped_single.reset_index()
-    grouped_single.columns = ['MUNICIPIO','DATA OB','Valor Total','Valor Minimo','Valor maximo']
-    grouped_single['Valor Total'] = grouped_single['Valor Total'].map('{:.2f}'.format)
-    teste = group.mean();
-    print(group.mean())
-    teste2[x] = grouped_single
-    ano_inicio = ano_inicio+1
+    pd.options.display.float_format = "{:.2f}".format
+    size = 10;
+    ano_inicio = 2009
+    teste2 = [0] * size
+    for x in range(size):
+        orcamento_mg = pd.read_csv("data/orcamento/orcamento_mg_{}.csv".format(ano_inicio), sep=';')
 
+        orcamento_mg["Valor Total"] = orcamento_mg["Valor Total"].replace('[.]', '', regex=True).replace('[,]', '.',
+                                                                                                         regex=True).replace(
+            '[R$]', '', regex=True).astype(float)
+        orcamento_mg["DATA OB"] = orcamento_mg["DATA OB"].apply(lambda x: x[3:10])
+        group = orcamento_mg['Valor Total'].groupby(orcamento_mg['MUNICIPIO'])
 
-result = pd.concat(teste2)
-result.to_csv(r'data/orcamento/orcamento_mg_consolidado.csv', index = False,sep=';')
+        grouped_single = orcamento_mg.groupby(['MUNICIPIO', 'DATA OB']).agg({'Valor Total': ['sum', 'min', 'max']})
+        grouped_single = grouped_single.reset_index()
+        grouped_single.columns = ['MUNICIPIO', 'DATA OB', 'Valor Total', 'Valor Minimo', 'Valor maximo']
+        grouped_single['Valor Total'] = grouped_single['Valor Total'].map('{:.2f}'.format)
+        teste = group.mean();
+        logger.info(group.mean())
+        teste2[x] = grouped_single
+        ano_inicio = ano_inicio + 1
+
+    result = pd.concat(teste2)
+    result.to_csv(r'data/staged_data/orcamento_mg_consolidado.csv', index=False, sep=';')
