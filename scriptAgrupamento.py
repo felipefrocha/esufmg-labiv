a62a8b4ca4bf0d4aae22e007d58bf2912c973e44
tiagolobato <53907497+tiagolobato@users.noreply.github.com>
2021-01-28 22:21:01 -0300

    Feature/junta scripts (#7)
    * juntar scripts
    
    * separa por minas

--- scriptAgrupamento.py
+++ scriptAgrupamento.py
@@ -13,14 +13,14 @@ enc = OneHotEncoder(handle_unknown='ignore')
 import pandas as pd
 size = 10;
 ano_inicio = 2009
-teste2 = [0] * size
 pd.options.display.float_format = '{:,.0f}'.format
 colsEnum=['CS_RACA','CS_SEXO','CS_GESTANT','CRITERIO']
 dadosTratadas = []
 
-dados = pd.read_csv("data/datasus/influd09_limpo_final.csv",sep=';',encoding = "ISO-8859-1")
+dados = pd.read_csv("consolidated_datasus.csv",sep=',',encoding = "ISO-8859-1")
 
 for num,col in enumerate(colsEnum, start=1):
+    print(col)
     enum = pd.read_csv("data/{}.csv".format(col),sep=';',encoding = "ISO-8859-1").columns
     dadosIter = dados
     enumTipo = dados[col]
@@ -35,7 +35,7 @@ for num,col in enumerate(colsEnum, start=1):
     
     dicionario = dict([(i, ['sum'])for i in enum])
     rnm_cols = dict(sum='Sum')
-    grouped_single = dadosIter.groupby(['ID_MUNICIP']).agg(dicionario).rename(columns=rnm_cols)
+    grouped_single = dadosIter.groupby(['ID_MUNICIP','DT_NOTIFIC','SG_UF_NOT']).agg(dicionario).rename(columns=rnm_cols)
 
 
     grouped_single.columns = enum 
@@ -45,7 +45,6 @@ for num,col in enumerate(colsEnum, start=1):
   
 
 #horizontal_stack = pd.concat([survey_sub, survey_sub_last10], axis=1)
-df_final = reduce(lambda left,right: pd.merge(left,right,on='ID_MUNICIP'), dadosTratadas).astype(int)
+df_final = reduce(lambda left,right: pd.merge(left,right,on=['ID_MUNICIP','DT_NOTIFIC','SG_UF_NOT']), dadosTratadas).astype(int)
 df_final.reset_index(level=0, inplace=True)
-#result = pd.concat(teste2)
-df_final.to_csv(r'data/dados_agrupados.csv', index = False,sep=';')
+df_final.to_csv(r'data/dados_agrupados.csv', index = True,sep=';')
