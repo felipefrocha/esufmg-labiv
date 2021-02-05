f376addf1f0c22755eeb5b959d00902ec8472ddf
Felipe F. Rocha <felipefonsecarocha@gmail.com>
2021-02-04 22:19:28 -0300

    Inclusão de pasta dvc

--- data_linkage.py
+++ data_linkage.py
@@ -35,28 +35,37 @@ def data_reader(path: any):
 
 
 def import_files():
-    sus = data_reader("data/staged_data/consolidated_datasus.csv")
+    sus = data_reader("data/staged_data/dados_agrupados.csv")
     budget = data_reader("data/staged_data/orcamento_mg_consolidado.csv")
+
+    logger.info("Treating index")
+
     sus['DT_NOTIFIC'] = sus['DT_NOTIFIC'].map(lambda x: x.replace('/',''))
     sus['ID_MUNICIP'] = sus['ID_MUNICIP'].map(lambda x: x.replace(' ',''))
-    budget['DT_NOTIFIC'] = sus['DT_NOTIFIC'].map(lambda x: x.replace('/',''))
-    budget['ID_MUNICIP'] = sus['ID_MUNICIP'].map(lambda x: x.replace(' ',''))
+
+    budget['DT_NOTIFIC'] = budget['DT_NOTIFIC'].map(lambda x: x.replace('/',''))
+    budget['ID_MUNICIP'] = budget['ID_MUNICIP'].map(lambda x: x.replace(' ',''))
+    
+    logger.info("Build index")
 
     sus['ID'] = sus['DT_NOTIFIC'] + sus['ID_MUNICIP']
     budget['ID'] = budget['DT_NOTIFIC'] + budget['ID_MUNICIP']
 
-
-
-
     sus.set_index(['ID'], inplace=True)
     budget.set_index(['ID'], inplace=True)
+
     sus.sort_index(inplace=True)
     budget.sort_index(inplace=True)
+
+    logger.info("Remove unecessary")
+    budget.drop(columns=['DT_NOTIFIC', 'ID_MUNICIP'], inplace=True)
+
     with open(file=f'{current_directory}/data/staged_data/dataliked.csv', mode='w', newline='') as csvfile:
-
+        
+        # logger.info(f'Teste de index: \n{sus.index.intersection(budget.index)}')
+        logger.info('Linking database')
         merged_data = pd.concat([sus, budget], axis=1, join="inner")
-        logger.info(f'Teste de index: \n{sus.index.intersection(budget.index)}')
+        
         merged_data.to_csv(path_or_buf=csvfile, index=False)
-
         logger.info("FINISH")
 
